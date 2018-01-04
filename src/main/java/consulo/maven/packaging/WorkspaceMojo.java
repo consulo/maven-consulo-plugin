8725c359f3609008c0f207281641e63ebf0980e2
Valeriy Semenchuk <vistall.valeriy@gmail.com>
2018-01-04 04:21:25 +0300

    if packaging is skipped - download plugins(in dev mode - we not build main artifact)

--- src/main/java/consulo/maven/packaging/WorkspaceMojo.java
+++ src/main/java/consulo/maven/packaging/WorkspaceMojo.java
@@ -40,7 +40,7 @@ public class WorkspaceMojo extends AbstractPackagingMojo
 	@Override
 	public void execute() throws MojoExecutionException, MojoFailureException
 	{
-		if(!myDependencies.isEmpty())
+		if(!myDependencies.isEmpty() && !packaging.skip)
 		{
 			createDependenciesWorkspace();
 		}
